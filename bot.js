fe46bbad78aeaaac8f91ef5f54303a327a0f9fdb
Shri <shrisharvesh7@gmail.com>
2023-08-15 20:31:53 +0800

    update

--- bot.js
+++ bot.js
@@ -1,79 +1,64 @@
-require('dotenv').config()
-const Twitch  = require("twitch-js");
-
+require('dotenv').config();
+const Twitch = require('twitch-js');
 const OBSWebSocket = require('obs-websocket-js');
+
 const obs = new OBSWebSocket();
+let obsScenes = [];
+let obsScenesOriginalName = [];
 
-let obsScenes = [],
-    obsScenesOriginalName = [];
-
-// Command
-const COMMAND_NAME = "!scene"; // Command to type in chat.
-
-// Check .env 
-if(process.env.TWITCH_CHANNEL === "") {
-  console.error(`You must enter a Twitch channel.`);
-  process.exit();
-}
-
-// Command cooldown
-const COMMAND_COOLDOWN = Number(process.env.COMMAND_COOLDOWN);
+const COMMAND_NAME = '!scene';
+const COMMAND_COOLDOWN = Number(process.env.COMMAND_COOLDOWN) || 10;
 let commandLastUsed = 0;
 
-// Connect to OBS
-obs.connect({ address: process.env.OBS_URL, password: process.env.OBS_PASSWORD })
-  .then(() => {
-    console.log(`[OBS] (log): Connected to OBS.`);
-    return obs.getSceneList();
-  })
-  .then(data => {
-    // Save scene names in lowercase and original name
+(async () => {
+  try {
+    await obs.connect({ address: process.env.OBS_URL, password: process.env.OBS_PASSWORD });
+    console.log('[OBS] (log): Connected to OBS.');
+    const data = await obs.getSceneList();
     obsScenesOriginalName = data.scenes.map(i => i.name);
     obsScenes = data.scenes.map(i => i.name.toLowerCase());
-  })
-  .catch(e => { 
-    console.log(`[OBS] (error): Could not connect to OBS.`); 
-    process.exit(); 
-  });
+  } catch (e) {
+    console.log('[OBS] (error): Could not connect to OBS.');
+    process.exit();
+  }
+})();
 
-const client = new Twitch.client({
-  channels: [`#${process.env.TWITCH_CHANNEL}`]
+const client = new Twitch.Client({
+  channels: [`#${process.env.TWITCH_CHANNEL}`],
 });
 
-// Twitch chat listener
-client.on('chat', (channel, userstate, message, self) => {
-
-  // Is user that's posting a mod or broadcaster?
-  if(Math.round((new Date()).getTime() / 1000) > commandLastUsed + COMMAND_COOLDOWN) {
-    if(userstate.badges != null && userstate.badges.hasOwnProperty("broadcaster") && userstate.badges.broadcaster === '1' || userstate.mod === true) {
+client.on('message', (channel, userstate, message, self) => {
+  if (Math.round(new Date().getTime() / 1000) > commandLastUsed + COMMAND_COOLDOWN) {
+    if (
+      (userstate.badges != null && userstate.badges.hasOwnProperty('broadcaster') && userstate.badges.broadcaster === '1') ||
+      userstate.mod === true
+    ) {
       message = message.toLowerCase();
-      if(message.indexOf(COMMAND_NAME) === 0) { // sent !scene command
-        message = message.split(" ");
-        if(message.length > 1) { // sent which scene to switch to
+      if (message.indexOf(COMMAND_NAME) === 0) {
+        message = message.split(' ');
+        if (message.length > 1) {
           message.shift();
-          message = message.join(" ");
-          if(obsScenes.indexOf(message) >= 0 ) { // scene with name exists, switch to it.
-            const sceneName = obsScenesOriginalName[obsScenes.indexOf(message)];
-            obs.setCurrentScene({'scene-name': sceneName});
-            console.log(`[OBS] (log): Switching to scene: ${sceneName}, command sent from ${userstate.username}.`);
-            commandLastUsed = Math.round((new Date()).getTime() / 1000);
+          const sceneName = message.join(' ');
+          const sceneIndex = obsScenes.indexOf(sceneName);
+          if (sceneIndex >= 0) {
+            const sceneNameOriginal = obsScenesOriginalName[sceneIndex];
+            obs.setCurrentScene({ 'scene-name': sceneNameOriginal });
+            console.log(`[OBS] (log): Switching to scene: ${sceneNameOriginal}, command sent from ${userstate.username}.`);
+            commandLastUsed = Math.round(new Date().getTime() / 1000);
           }
         }
       }
     }
   }
-  
 });
 
 obs.on('error', err => {
-  console.error(`[OBS] (error):`, err);
+  console.error('[OBS] (error):', err);
 });
 
-//When OBS exits, so does the bot
-obs.on("Exiting", () => {
-  console.log("[OBS] (Exit): Good work streaming");
+obs.on('Exiting', () => {
+  console.log('[OBS] (Exit): Good work streaming');
   process.exit();
 });
 
-// Connect to twitch chat
 client.connect();
